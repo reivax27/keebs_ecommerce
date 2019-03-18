365a70c8d27a96c3d0a27aa7f3364f4b3e94ce9b
Xavier Macalalad <johnxavierm47@gmail.com>
2019-03-18 11:47:36 -0500

    Created a Category table/model and added an association with a foreign key to the Product table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_161642) do
+ActiveRecord::Schema.define(version: 2019_03_18_163458) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_03_18_161642) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
