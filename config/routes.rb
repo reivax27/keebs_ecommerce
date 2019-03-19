df6032b3d8d4c2b1a8365258e32bd60df392040b
Xavier Macalalad <johnxavierm47@gmail.com>
2019-03-19 13:37:05 -0500

    Generated a controller with an index and show action view for Products, Categories, Customers, and Orders model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/show'
+  get 'customers/index'
+  get 'customers/show'
+  get 'categories/index'
+  get 'categories/show'
+  get 'products/index'
+  get 'products/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
