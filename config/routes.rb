f314e6253ac6aeee50907803e0b89617e456b360
Xavier Macalalad <johnxavierm47@gmail.com>
2019-03-21 09:06:54 -0500

    Configured the view nd controller for Products and Categories. Also uploaded a few products with images associated with them.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/show'
-  get 'customers/index'
-  get 'customers/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/index'
-  get 'products/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index, :show]
+  resources :categories, only: [:index, :show]
+
+  root to: 'products#index'
+
 end
