7d3a89a10e8f49466629046b9f9aac11fc767d83
Xavier Macalalad <johnxavierm47@gmail.com>
2019-03-26 11:07:04 -0500

    Modified my navbar to accomodate for the sign up and login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
 
   protect_from_forgery
-  
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :name, :address, :phone, :province_id)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :current_password, :name, :address, :phone, :province_id)}
+  end
 end
