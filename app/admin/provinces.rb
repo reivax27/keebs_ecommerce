0a2e656c63f28ea141a229567d7ffe291bb95ded
Xavier Macalalad <johnxavierm47@gmail.com>
2019-04-04 11:47:41 -0500

    Added the new tax columns to be whitelisted for active admin. Also added all tax information based on the province.

--- app/admin/provinces.rb
+++ app/admin/provinces.rb
@@ -11,5 +11,5 @@ ActiveAdmin.register Province do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-  permit_params :name
+  permit_params :name, :gst, :pst, :hst
 end
